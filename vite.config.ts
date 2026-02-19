43ca8b1965b92de9c8d4ec908c567314862ee6b3
MiteshOP16 <miteshuttekar16@gmail.com>
2026-02-19 05:45:53 +0530

    changes in host name

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: true,
+    host: "renvo-ai.onrender.com",
     port: 8080,
      allowedHosts: [
       "renvo-ai.onrender.com"
